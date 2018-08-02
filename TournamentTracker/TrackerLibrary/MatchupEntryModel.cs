3a8c5d652441ff7a25f05006b134b46342cfb8f3
vesnenokanna <a.vesnenok@student.fontys.nl>
2018-08-02 21:38:46 +0200

    created a VS project and created a class library

---
+++ TournamentTracker/TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+   public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Represents one Team in the matchup
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Represents the score for this particular team
+        /// </summary>
+        public double Score { get; set; }
+
+        /// <summary>
+        /// Represents the matchup this team came 
+        /// from as the winner
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+
+    }
+}
